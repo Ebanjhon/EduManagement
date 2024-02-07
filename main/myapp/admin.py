86f4a8740e9c02a0c2a57d59e887921b1088b5ad
Ebanjhon <yjhoneban1403@gmail.com>
2024-02-07 15:23:29 +0700

    edit admin site

--- main/myapp/admin.py
+++ main/myapp/admin.py
@@ -1,10 +1,45 @@
 from django.contrib import admin
 from .models import User, StudyClass, Semester, ScoreColumn, ResultLearning, Course
 
-admin.site.register(User)
-admin.site.register(ScoreColumn)
-admin.site.register(StudyClass)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["id","id_user", "username", "first_name", "last_name", "role"]
+    search_fields = ["id_user", "username"]
+    list_filter = ["role"]
+
+class ResultLearningAdmin(admin.ModelAdmin):
+    list_display = ["student", "study_class", "midterm_score", "final_score"]
+    search_fields = ('student__first_name', 'student__last_name', 'study_class__name')
+    list_filter = ["study_class"]
+
+class StudyClassAdmin(admin.ModelAdmin):
+    list_display = ('name', 'semester', 'teacher', 'course')
+    search_fields = ('name', 'teacher__first_name', 'course__name')
+    list_filter = ('semester', 'course', 'teacher')
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "credit_hours"]
+    search_fields = ["id", "name"]
+    list_filter = ["name"]
+
+class ScoreColumnAdmin(admin.ModelAdmin):
+    list_display = ('name_column', 'display_student', 'score', 'display_result_learning')
+    search_fields = ('name_column', 'result_learning__study_class__name', 'result_learning__student__first_name', 'result_learning__student__last_name')
+    list_filter = ('result_learning__study_class', 'result_learning__student')
+
+    def display_student(self, obj):
+        # Trả về tên đầy đủ của sinh viên
+        return obj.result_learning.student.get_full_name()
+    display_student.short_description = 'Student'
+
+    def display_result_learning(self, obj):
+        # Trả về thông tin về lớp học và điểm
+        study_class_name = obj.result_learning.study_class.name
+        return f"{study_class_name}"
+    display_result_learning.short_description = 'Class Information'
+
+admin.site.register(User, UserAdmin)
+admin.site.register(ScoreColumn, ScoreColumnAdmin)
+admin.site.register(StudyClass, StudyClassAdmin)
 admin.site.register(Semester)
-admin.site.register(ResultLearning)
-admin.site.register(Course)
-# admin.site.register(StudyClass)
+admin.site.register(ResultLearning, ResultLearningAdmin)
+admin.site.register(Course, CourseAdmin)
--- main/myapp/models.py
+++ main/myapp/models.py
@@ -8,7 +8,7 @@ class User(AbstractUser):
         ADMIN = 'admin'
         TEACHER = 'teacher'
         STUDENT = 'student'
-
+        
     role = models.CharField(max_length=10, choices=UserRole.choices, default=UserRole.STUDENT)
     id_user = models.IntegerField(unique=False, blank=True, null=True)
     birth_date = models.DateField(null=True)
@@ -20,6 +20,9 @@ class User(AbstractUser):
             count = User.objects.filter(role=self.role).count()
             self.id_user = count + 1
         super(User, self).save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.first_name +" "+ self.last_name
 
 
 # cac models khac 
@@ -53,6 +56,8 @@ class StudyClass(ModelBase):
     semester = models.ForeignKey(Semester, on_delete=models.CASCADE)#nam hoc ky
     teacher = models.ForeignKey(User, on_delete=models.CASCADE, related_name='classrooms_as_teacher', limit_choices_to={'role': User.UserRole.TEACHER})
     students = models.ManyToManyField(User, related_name='classrooms_as_student', limit_choices_to={'role': User.UserRole.STUDENT})
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.name
     
