d50214a947e442dcf2602eea065067b954e144e5
BEOBUJ <trackmix115@gmail.com>
2024-02-18 18:54:56 +0700

    fix API

--- main/main/urls.py
+++ main/main/urls.py
@@ -1,10 +1,8 @@
-
 from django.contrib import admin
 from django.urls import path, include, re_path
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -14,10 +12,9 @@ schema_view = get_schema_view(
         contact=openapi.Contact(email="thanh.dh@ou.edu.vn"),
         license=openapi.License(name="Dương Hữu Thành@2021"),
     ),
-        public=True,
-        permission_classes=(permissions.AllowAny,),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,5 +22,6 @@ urlpatterns = [
     path('myapp/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 ]
